4f9ad950a6d439e33422a1f34b3e5a78ac522627
Timothee Cezard <tcezard@ebi.ac.uk>
2020-06-05 10:59:52 +0100

    Checks the VCF content in the test

--- tests/test_pipeline.sh
+++ tests/test_pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -Eeuxo pipefail
+set -Eeuo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SOURCE_DIR=$(dirname $SCRIPT_DIR)
@@ -23,5 +23,15 @@ ls ${SCRIPT_DIR}/resources/remap.vcf \
    ${SCRIPT_DIR}/resources/new_genome.fa.fai \
    ${SCRIPT_DIR}/resources/genome.fa.fai
 
+# Build the expected VCF
+cat << EOT > "${SCRIPT_DIR}/resources/expected_remap.vcf"
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO
+chr2	98	.	C	G	50	PASS	.
+chr2	1078	.	G	A	50	PASS	.
+EOT
+
+# Compare vs the expected VCF
+diff "${SCRIPT_DIR}/resources/expected_remap.vcf" <(grep -v '^##' "${SCRIPT_DIR}/resources/remap.vcf")
+
 # Clean up after the test
-rm -rf work .nextflow* ${SCRIPT_DIR}/resources/remap.vcf* ${SCRIPT_DIR}/resources/new_genome.fa.* ${SCRIPT_DIR}/resources/genome.fa.fai
+rm -rf work .nextflow* ${SCRIPT_DIR}/resources/*remap.vcf* ${SCRIPT_DIR}/resources/new_genome.fa.* ${SCRIPT_DIR}/resources/genome.fa.fai
