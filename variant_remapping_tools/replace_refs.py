75f0b2bdb37ba4e46aea33d08e74a97a912d668f
Timothee Cezard <tcezard@ebi.ac.uk>
2021-02-24 17:52:17 +0000

    make line number 1 based instead of 0 based

--- variant_remapping_tools/replace_refs.py
+++ variant_remapping_tools/replace_refs.py
@@ -7,9 +7,9 @@ import linecache
 def process_variant(line, line_num, oldrefalleles_file):
     variant = line.split('\t')
     # Gets corresponding REF allele from the oldrefalleles file.
-    old_ref = linecache.getline(oldrefalleles_file, line_num + 1).rstrip()
+    old_ref = linecache.getline(oldrefalleles_file, line_num).rstrip()
     # The reference allele extracted from the new genome in insertReferenceAllele and stored in variant[3]
-    # is only one base long. There test "variant[3] == variant[4]" can only work if the ref and alt have
+    # is only one base long. The test "variant[3] == variant[4]" can only work if the ref and alt have
     # the same length which won't be the case for indels.
     # For insertion, we're leaving the single base extracted from the new genome (regardless of the alt)
     # for deletion, we're using the old reference sequence (regardless of the alt)
@@ -18,6 +18,7 @@ def process_variant(line, line_num, oldrefalleles_file):
         return '\t'.join((str(e) for e in [variant[0], variant[1], variant[2], old_ref, variant[4], variant[5], variant[6], variant[7]]))
     else:
         return line
+
 
 if __name__ == "__main__":
     description = "replaces the REF allele with the old REF allele if REF = ALT in the remapped VCF"
@@ -34,4 +35,4 @@ if __name__ == "__main__":
             if line.startswith('#'):
                 out_vcf.write(line)
             else:
-                out_vcf.write(process_variant(line, i, args.oldrefalleles))
+                out_vcf.write(process_variant(line, i + 1, args.oldrefalleles))
--- variant_remapping_tools/tests/test_replace_refs.py
+++ variant_remapping_tools/tests/test_replace_refs.py
@@ -10,7 +10,7 @@ class TestReplaceRef(TestCase):
         # Reference allele from new genome was inserted in the vcf file and as a result the ref and alt are the same
         line = '\t'.join(['chr1', '1', '.', 'G', 'G', '.', '.', '.'])
         oldrefalleles_file = os.path.join(os.path.dirname(__file__), 'oldrefalleles.txt')
-        line_num = 0
+        line_num = 1
         # The old ref allele was reinserted
         assert process_variant(line, line_num, oldrefalleles_file) == 'chr1	1	.	C	G	.	.	.'
         # Note that I think this is wrong: the old ref allele should have been inserted in the ALT not in REF column
@@ -19,7 +19,7 @@ class TestReplaceRef(TestCase):
         # Reference allele from new genome was inserted in the file which make the deletion disappear
         line = '\t'.join(['chr2', '1', '.', 'T', 'C', '.', '.', '.'])
         oldrefalleles_file = os.path.join(os.path.dirname(__file__), 'oldrefalleles.txt')
-        line_num = 1
+        line_num = 2
         # The old ref allele was reinserted because the second line in oldref file is TAG
         assert process_variant(line, line_num, oldrefalleles_file) == 'chr2	1	.	TAG	C	.	.	.'
 
@@ -27,5 +27,5 @@ class TestReplaceRef(TestCase):
         # The bases are different so no changes is applied
         line = '\t'.join(['chr1', '1', '.', 'T', 'G', '.', '.', '.'])
         oldrefalleles_file = os.path.join(os.path.dirname(__file__), 'oldrefalleles.txt')
-        line_num = 0
+        line_num = 1
         assert process_variant(line, line_num, oldrefalleles_file) == 'chr1	1	.	T	G	.	.	.'
